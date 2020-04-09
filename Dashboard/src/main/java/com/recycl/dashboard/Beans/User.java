6e03e74b443da9afe67515d0c0430d093507d150
LouBege <wolfla@hotmail.fr>
2020-04-09 11:49:26 +0200

    feat : add beans role and user

---
+++ Dashboard/src/main/java/com/recycl/dashboard/Beans/User.java
@@ -0,0 +1,50 @@
+package com.recycl.dashboard.Beans;
+
+public class User {
+    private int id = 0;
+    private String username = "";
+    private String password = "";
+    private Role role = null;
+
+    public User(int id, String username, String password, Role role) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public User() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
