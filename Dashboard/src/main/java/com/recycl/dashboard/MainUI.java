eaeb91101acb2370c323b5bd9b90466e2a1b1ae2
Adrien RICHARD <richardadrien0@gmail.com>
2020-04-13 16:15:32 +0200

    first scenes

---
+++ Dashboard/src/main/java/com/recycl/dashboard/MainUI.java
@@ -0,0 +1,80 @@
+package com.recycl.dashboard;
+
+import eu.hansolo.tilesfx.Tile;
+import eu.hansolo.tilesfx.TileBuilder;
+import eu.hansolo.tilesfx.skins.BarChartItem;
+import javafx.animation.AnimationTimer;
+import eu.hansolo.tilesfx.skins.BarChartItem;
+
+public class MainUI {
+    private static final double TILE_SIZE=150;
+    private static final Random RND=new Random();
+    private Tile barChartTile;
+    private long lastTimerCall;
+    private AnimationTimer timer;
+
+
+    @Override
+    public void init() {
+
+        BarChartItem barChartItem1 = new BarChartItem("mark", 47, Tile.BLUE);
+        BarChartItem barChartItem2 = new BarChartItem("Adrien", 47, Tile.RED);
+        BarChartItem barChartItem3 = new BarChartItem("edouard", 47, Tile.GREEN);
+        BarChartItem barChartItem4 = new BarChartItem("lou", 47, Tile.ORANGE);
+
+        barChartItem1.setFormatString("%.2f");
+        barChartItem2.setFormatString("%.2f");
+        barChartItem3.setFormatString("%.2f");
+        barChartItem4.setFormatString("%.2f");
+
+        barChartTile = TileBuilder.create()
+                .prefSize(TILE_SIZE, TILE_SIZE)
+                .skinType(Tile.SkinType.BAR_CHART)
+                .title("BarChart Tile")
+                .text("Whatever text")
+                .barChartItems(barChartItem1, barChartItem2, barChartItem3, barChartItem4)
+                .decimals(0)
+                .build();
+        lastTimerCall = System.nanoTime();
+        timer = new AnimationTimer() {
+            @Override
+            public void handle(final long now) {
+                if (now > lastTimerCall + 1_000_000_000) {
+
+                    barChartTile.getBarChartItems().get(RND.nextInt(3)).setValue(RND.nextDouble() * 80);
+
+                    lastTimerCall = now;
+                }
+            }
+        };
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        try {
+            Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("login.fxml"));
+            Scene scene = new Scene(root, 640, 480);
+           /* Scene scene = new Scene(root, 640, 480);
+            stage.setTitle("RECYCL Dashboard");
+            stage.setScene(scene);
+            stage.show();
+            timer.start();*/
+
+            ScreenController screenController = new ScreenController(scene);
+            // screenController.addScreen("login", FXMLLoader.load(getClass().getResource("login.fxml")));
+            //  screenController.addScreen("main", FXMLLoader.load(getClass().getResource( "main.fxml" )));
+            // screenController.addScreen("test", FXMLLoader.load(getClass().getResource( "test.fxml" )));
+            screenController.activate("login");
+
+        } catch (Exception ex) {
+            System.out.println(ex);
+            stop();
+        }
+    }
+
+    @Override
+    public void stop() {
+        System.exit(0);
+    }
+}
+
---
+++ Dashboard/src/main/java/com/recycl/dashboard/controllers/LoginController.java
@@ -0,0 +1,43 @@
+package com.recycl.dashboard.controllers;
+
+import com.recycl.dashboard.helpers.AlertHelper;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.stage.Window;
+
+public class LoginController {
+    @FXML
+    public Pane TestPane;
+
+    @FXML
+    public TextField loginField;
+
+    @FXML
+    public Button submitButton;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    protected void handleSubmitButtonAction(ActionEvent event) {
+        Window owner = submitButton.getScene().getWindow();
+        if (loginField.getText().isEmpty()) {
+            AlertHelper.showAlert(Alert.AlertType.ERROR, owner, "Form Error!",
+                    "Please enter your name");
+            return;
+        }
+        if (passwordField.getText().isEmpty()) {
+            AlertHelper.showAlert(Alert.AlertType.ERROR, owner, "Form Error!",
+                    "Please enter a password");
+            return;
+        }
+
+        AlertHelper.showAlert(Alert.AlertType.CONFIRMATION, owner, "Registration Successful!",
+                "Welcome " + loginField.getText());
+    }
+}
