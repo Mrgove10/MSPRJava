6c9d71223a911e19813920de4497276fa819192a
Adrien RICHARD <richardadrien0@gmail.com>
2020-04-13 17:04:22 +0200

    [fix] login hello world

--- Dashboard/src/main/java/com/recycl/dashboard/Main.java
+++ Dashboard/src/main/java/com/recycl/dashboard/Main.java
@@ -4,6 +4,8 @@ import com.recycl.dashboard.back.MainBDD;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
@@ -18,19 +20,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("Hello World!");
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.setTitle("RECYCL Dashboard");
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("login.fxml"));
+        Scene scene = new Scene(root, 640, 480);
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 }
