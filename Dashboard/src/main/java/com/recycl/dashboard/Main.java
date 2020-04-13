389f3c5569061ca31d756db590b3d56a93bf4679
Adrien RICHARD <richardadrien0@gmail.com>
2020-04-13 17:34:11 +0200

    added bcrypt and clean

--- Dashboard/src/main/java/com/recycl/dashboard/Main.java
+++ Dashboard/src/main/java/com/recycl/dashboard/Main.java
@@ -1,11 +1,15 @@
 package com.recycl.dashboard;
 
 import com.recycl.dashboard.back.MainBDD;
+import com.recycl.dashboard.front.helpers.AlertHelper;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.stage.Stage;
+
+import java.util.Objects;
 
 public class Main extends Application {
     public static void main(String[] args) {
@@ -15,15 +19,16 @@ public class Main extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) {
         try {
             primaryStage.setTitle("RECYCL Dashboard");
-            Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("login.fxml"));
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("login.fxml")));
             Scene scene = new Scene(root, 640, 480);
             primaryStage.setScene(scene);
             primaryStage.show();
         } catch (Exception e) {
             e.printStackTrace();
+            AlertHelper.showAlert(Alert.AlertType.ERROR, primaryStage, "Error", e.toString());
         }
     }
 }
