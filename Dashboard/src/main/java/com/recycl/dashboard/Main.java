78faff13cc67e04be4fa8f79dc2f52eb5a919a2f
LouBege <wolfla@hotmail.fr>
2020-04-13 16:53:18 +0200

    feat : user DAO works

--- Dashboard/src/main/java/com/recycl/dashboard/Main.java
+++ Dashboard/src/main/java/com/recycl/dashboard/Main.java
@@ -1,14 +1,14 @@
 package com.recycl.dashboard;
 
 import com.recycl.dashboard.back.MainBDD;
-import com.recycl.dashboard.front.MainUI;
+//import com.recycl.dashboard.front.MainUI;
 
 public class Main {
     public static void main(String[] args) {
         MainBDD bdd =new MainBDD();
         bdd.startBDD();
-        MainUI ui = new MainUI();
-        ui.init();
+//        MainUI ui = new MainUI();
+//        ui.init();
     }
 }
 
